0d4695a9d28a08b0f1895d4d3ee215d1f76a244e
Rafael <rafaelmagalhaes1@live.com>
2023-11-26 09:50:11 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <>
-      Cell Store
-    </>
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route index element={ <Home /> } />
+      </Route>
+    </Routes>
   );
 }
 
