a9440a72bd4acf7d5c296bcba400171517fdc8e4
Rafael <rafaelmagalhaes1@live.com>
2023-11-26 17:16:05 -0300

    feat: create Global Style

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './styles/GlobalStyle.tsx';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
