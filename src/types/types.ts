e7decb1736f3727691d1a14dd77e6120a3598dfa
Rafael <rafaelmagalhaes1@live.com>
2023-11-26 17:04:48 -0300

    feat: create product Type

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type ProductType = {
+  id: string;
+  title: string;
+  price: number;
+  thumbnail: string;
+  condition: string;
+  available_quantity: number;
+  seller: {
+    nickname: string;
+  };
+};
