c083281b9395213a45ac3df60733a6f966fd6c9f
Rafael <rafaelmagalhaes1@live.com>
2023-11-27 19:04:36 -0300

    feat: add product provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle.tsx';
 import App from './App.tsx';
+import ProductProvider from './context/ProductProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <ProductProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ProductProvider>
   </React.StrictMode>,
 );
