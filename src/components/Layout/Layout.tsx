015af88e75b56039135c33d95f7bc7887f6fd596
Rafael <rafaelmagalhaes1@live.com>
2023-11-27 08:47:04 -0300

    feat: add components in Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
+import NavBarTop from '../NavBarTop/NavBarTop';
 
 function Layout() {
   return (
     <>
+      <NavBarTop />
       <Header />
+      <NavBar />
       <Outlet />
     </>
   );
