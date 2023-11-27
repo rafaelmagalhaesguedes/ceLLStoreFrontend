15985f06c293acd470f3e457da010e9ca002e2fd
Rafael <rafaelmagalhaes1@live.com>
2023-11-27 08:47:28 -0300

    fix: backgroud color Container master

--- src/pages/Styles.tsx
+++ src/pages/Styles.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  background-color: #1e1e1e;
   display: flex;
   flex-direction: column;
   justify-content: center;
