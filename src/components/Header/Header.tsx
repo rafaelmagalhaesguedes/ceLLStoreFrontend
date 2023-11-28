09675c21fd560184d941ecf510aa913c0b1e4bd3
Rafael <rafaelmagalhaes1@live.com>
2023-11-28 14:24:32 -0300

    fix: refact searchbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,14 @@
-import { faCartShopping, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useContext, useState } from 'react';
-import { HeaderContainer, HeaderWrapper, SearchBar } from './Styles';
-import ProductContext from '../../context/ProductContext';
+import { HeaderContainer, HeaderWrapper, Logo } from './Styles';
+import SearchBar from '../SearchBar/SearchBar';
 
 function Header() {
-  const { filterByString } = useContext(ProductContext);
-  const [search, setSearch] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  const handleClick = () => {
-    filterByString(search);
-    setSearch('');
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      filterByString(search);
-      setSearch('');
-    }
-  };
-
   return (
     <HeaderContainer>
       <HeaderWrapper>
-        <div>
-          <h1>Cell Store</h1>
-        </div>
-        <SearchBar>
-          <input
-            type="text"
-            placeholder="Buscar..."
-            value={ search }
-            onChange={ handleChange }
-            onKeyDown={ handleKeyDown }
-          />
-          <FontAwesomeIcon
-            onClick={ handleClick }
-            icon={ faSearch }
-          />
-        </SearchBar>
+        <Logo><h1>Cell Store</h1></Logo>
+        <SearchBar />
         <FontAwesomeIcon icon={ faCartShopping } size="2x" />
       </HeaderWrapper>
     </HeaderContainer>
--- src/components/Header/Styles.tsx
+++ src/components/Header/Styles.tsx
@@ -18,34 +18,5 @@ export const HeaderWrapper = styled.div`
   align-items: center;
 `;
 
-export const SearchBar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 0.5rem;
-  border: 1px solid #999;
-  width: 40%;
-  padding: 0.1rem 0.5rem;
-
-  input {
-    width: 100%;
-    border: none;
-    background-color: transparent;
-    color: #fff;
-    border: none;
-    padding: 0.5rem;
-  }
-
-  :focus {
-    outline: none;
-    border: none;
-  }
-
-  button {
-    border: none;
-    background-color: transparent;
-    color: #fff;
-    font-weight: bold;
-    cursor: pointer;
-  }
+export const Logo = styled.div`
 `;
