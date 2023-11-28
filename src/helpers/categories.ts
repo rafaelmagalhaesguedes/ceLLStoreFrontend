09f818410e42a85fc84918fc757a21b77cba83fe
Rafael <rafaelmagalhaes1@live.com>
2023-11-28 07:47:29 -0300

    feat: create helper categories data

---
+++ src/helpers/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 'MLB1051',
+    name: 'Celulares e Telefones',
+  },
+  {
+    id: 'MLB1039',
+    name: 'Câmeras e Acessórios',
+  },
+  {
+    id: 'MLB1648',
+    name: 'Informática',
+  },
+  {
+    id: 'MLB1144',
+    name: 'Games',
+  },
+];
