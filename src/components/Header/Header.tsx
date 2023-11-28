63eb76c3c1006ea523d50a3b053508928292766b
Rafael <rafaelmagalhaes1@live.com>
2023-11-28 07:47:57 -0300

    feat: create filter by string on searchbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,29 @@
 import { faCartShopping, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext, useState } from 'react';
 import { HeaderContainer, HeaderWrapper, SearchBar } from './Styles';
+import ProductContext from '../../context/ProductContext';
 
 function Header() {
+  const { filterByString } = useContext(ProductContext);
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClick = () => {
+    filterByString(search);
+    setSearch('');
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      filterByString(search);
+      setSearch('');
+    }
+  };
+
   return (
     <HeaderContainer>
       <HeaderWrapper>
@@ -10,8 +31,17 @@ function Header() {
           <h1>Cell Store</h1>
         </div>
         <SearchBar>
-          <input type="text" placeholder="Buscar..." />
-          <FontAwesomeIcon icon={ faSearch } />
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={ search }
+            onChange={ handleChange }
+            onKeyDown={ handleKeyDown }
+          />
+          <FontAwesomeIcon
+            onClick={ handleClick }
+            icon={ faSearch }
+          />
         </SearchBar>
         <FontAwesomeIcon icon={ faCartShopping } size="2x" />
       </HeaderWrapper>
