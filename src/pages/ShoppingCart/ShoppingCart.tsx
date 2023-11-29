7be9ca6232009351259c033e70651adff818ff7b
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 19:21:00 -0300

    refact: shopping cart, remove props

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,17 +1,12 @@
-/* eslint-disable no-alert */
-import { useContext } from 'react';
-import ProductContext from '../../context/ProductContext';
 import { CartContainer } from './Styles';
 import CartCard from '../../components/CartCard/CartCard';
 import CartCheckout from '../../components/CartCheckout/CartCheckout';
 
 function ShoppingCart() {
-  const { cart } = useContext(ProductContext);
-
   return (
     <CartContainer>
-      <CartCard cart={ cart } />
-      <CartCheckout cart={ cart } />
+      <CartCard />
+      <CartCheckout />
     </CartContainer>
   );
 }
