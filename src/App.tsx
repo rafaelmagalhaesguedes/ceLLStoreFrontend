c01720bae14a818f7995e31f14133b9c2b74230b
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 16:56:56 -0300

    feat: add route to Shopping Cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
         <Route index element={ <Home /> } />
+        <Route path="/shopping-cart" element={ <ShoppingCart /> } />
       </Route>
     </Routes>
   );
