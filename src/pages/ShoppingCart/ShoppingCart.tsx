eab6ac2d947f4e330963f2950bac06dade5df5d7
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 16:54:15 -0300

    feat: create Shopping Cart page

---
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,124 @@
+/* eslint-disable no-alert */
+import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReply, faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import ProductContext from '../../context/ProductContext';
+import {
+  ButtonFinish, Cart, CartCheckout, CartContainer, CartInfo, Subtotal } from './Styles';
+
+function ShoppingCart() {
+  const { cart, setCart } = useContext(ProductContext);
+  const navigate = useNavigate();
+
+  const cartTotal = cart
+    .reduce((acc: number, item: any) => acc + item.price * item.quantity, 0);
+
+  const cartTotalFormatted = cartTotal
+    .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+  const increaseQuantity = (itemId: string) => {
+    const item = cart.find((cartItem) => cartItem.id === itemId);
+    if (item) {
+      // Check if the quantity is less than the available quantity
+      if (item.quantity < item.available_quantity) {
+        item.quantity += 1;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        setCart([...cart]); // Update cart in context
+      } else {
+        // Show an error message
+        alert('You cannot add more of this item, as it exceeds the available quantity.');
+      }
+    }
+  };
+
+  const decreaseQuantity = (itemId: string) => {
+    const item = cart.find((cartItem) => cartItem.id === itemId);
+    if (item && item.quantity > 1) {
+      item.quantity -= 1;
+      localStorage.setItem('cart', JSON.stringify(cart));
+      setCart([...cart]); // Update cart in context
+    }
+  };
+
+  return (
+    <CartContainer>
+      <Cart>
+        <button type="button" className="button" onClick={ () => navigate('/') }>
+          <FontAwesomeIcon icon={ faReply } size="1x" />
+          {' '}
+          Continuar comprando
+        </button>
+        <CartInfo>
+          <h1>Carrinho de compras</h1>
+          <ul>
+            {cart.map((item) => (
+              <li key={ item.id }>
+                <img src={ item.thumbnail } alt={ item.title } />
+                <h3>{item.title}</h3>
+                <div>
+                  <FontAwesomeIcon
+                    onClick={ () => decreaseQuantity(item.id) }
+                    icon={ faMinusCircle }
+                    size="lg"
+                  />
+                  <span>{item.quantity}</span>
+                  <FontAwesomeIcon
+                    onClick={ () => increaseQuantity(item.id) }
+                    icon={ faPlusCircle }
+                    size="lg"
+                  />
+                </div>
+                <p>
+                  R$
+                  {' '}
+                  {item.price}
+                </p>
+                <button>
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+          {cart.length === 0 && <p>Não há produtos no carrinho</p>}
+        </CartInfo>
+      </Cart>
+      <CartCheckout>
+        <h2>Resumo do pedido</h2>
+        {cart.map((item) => (
+          <div key={ item.id }>
+            <h3>
+              {item.title}
+              {' '}
+            </h3>
+            <p>
+              {item.shipping.free_shipping ? '(Frete grátis)' : ''}
+            </p>
+            <p>
+              Quantidade:
+              {' '}
+              {item.quantity}
+              {' '}
+            </p>
+            <p>
+              Total:
+              {' '}
+              {(item.price * item.quantity)
+                .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+
+            </p>
+          </div>
+        ))}
+        <Subtotal>
+          <p>Subtotal</p>
+          <p>{cartTotalFormatted}</p>
+        </Subtotal>
+        <ButtonFinish type="button">
+          Finalizar compra
+        </ButtonFinish>
+      </CartCheckout>
+    </CartContainer>
+  );
+}
+
+export default ShoppingCart;
---
+++ src/pages/ShoppingCart/Styles.tsx
@@ -0,0 +1,182 @@
+import styled from 'styled-components';
+
+export const CartContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1.5rem;
+  justify-content: space-between;
+  width: 80%;
+  gap: 2rem;
+  margin: 0 auto;
+`;
+
+export const Cart = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  padding: 1.5rem;
+  background-color: #fff;
+
+  .button {
+    background-color: transparent;
+    border: none;
+    border-radius: 5px;
+    color: #000;
+    cursor: pointer;
+    font-size: 1rem;
+    margin-bottom: 20px;
+    align-self: flex-start;
+    margin-bottom: 1.5rem;
+    
+    &:hover {
+      background-color: #bbb;
+    }
+  }
+`;
+
+export const CartInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    list-style: none;
+    width: 100%;
+
+    li {
+      align-items: center;
+      justify-content: space-between;
+      border-top: 1px solid #ccc;
+      display: flex;
+      flex-direction: row;
+      padding: 30px 0;
+      gap: 2rem;
+      width: 100%;  
+
+      img {
+        border-radius: 5px;
+        width: 100%;
+      }
+
+      h3 {
+        width: 100%;
+        font-size: 1rem;
+        text-align: left;
+        color: #444;
+      }
+
+      div {
+        display: flex;
+        flex-direction: row;
+        width: 50%;
+        padding: 0 20px;
+        justify-content: space-between;
+        align-items: center;
+        gap: 1rem;
+
+        button {
+          margin-top: 3px;
+        }
+      }
+
+      p {
+        width: 50%;
+        font-size: 1rem;
+        text-align: center;
+        color: #f80;
+        font-weight: bold;
+      }
+
+      button {
+        background-color: transparent;
+        border: none;
+        border-radius: 5px;
+        color: #000;
+        cursor: pointer;
+        font-size: 1rem;
+        margin-bottom: 20px;
+        align-self: flex-start;
+        margin-bottom: 1.5rem;
+        width: 10%;
+        
+        &:hover {
+          background-color: #bbb;
+        }
+      }
+    }
+  }
+`;
+
+export const CartCheckout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  align-items: center;
+  padding: 2.5rem;
+  background-color: #fff;
+  height: auto;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+  div {
+    border-top: 1px solid #ccc;
+    padding: 10px 0;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 20px;
+  }
+
+  h3 {
+    font-size: 1rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 0.8rem;
+    padding: 0.5rem 0;
+    color: #444;
+  }
+`;
+
+export const Subtotal = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding: 10px 0;
+  
+    p {
+        font-size: 1rem;
+        font-weight: bold;
+    }
+`;
+
+export const ButtonFinish = styled.button`
+    background-color: #f80;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    cursor: pointer;
+    font-size: 1rem;
+    margin-bottom: 20px;
+    align-self: flex-start;
+    margin-bottom: 1.5rem;
+    width: 100%;
+    padding: 10px 0;
+    font-weight: bold;
+    
+    &:hover {
+        background-color: #e70;
+    }
+    `;
