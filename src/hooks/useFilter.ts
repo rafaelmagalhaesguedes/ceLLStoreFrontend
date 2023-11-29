41335b1152ba87b3d722014313264b9da559a4ea
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 08:16:27 -0300

    refact: useFilter hook

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,39 +1,25 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { ProductType } from '../types/types';
 
-type SetProductType = any;
-type SetLoadingType = (loading: boolean) => void;
-
-function useFilter(
-  setProduct: SetProductType,
-  product: ProductType[],
-  setLoading: SetLoadingType,
-) {
+function useFilter(product: ProductType[], setProduct: (product: ProductType[]) => void) {
   //
   // Filter by free shipping
   const filterFreeShipping = () => {
-    setLoading(true);
     const filtered = product.filter((item) => item.shipping.free_shipping);
     setProduct(filtered);
-    setLoading(false);
     return filtered;
   };
   //
   // Filter by most expensive
   const filterMostExpensive = () => {
-    setLoading(true);
     const sorted = [...product].sort((a, b) => b.price - a.price);
     setProduct(sorted);
-    setLoading(false);
     return sorted;
   };
   //
   // Filter by cheapest
   const filterCheapest = () => {
-    setLoading(true);
     const sorted = [...product].sort((a, b) => a.price - b.price);
     setProduct(sorted);
-    setLoading(false);
     return sorted;
   };
 
