fd14bcb56dc2e315bf30f143f36e50b15fef529e
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 16:55:01 -0300

    fix: background color main container

---
+++ src/pages/Home/Styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #1e1e1e;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
