9337db37a6a2f82eba521de76298395f76cde992
Rafael <rafaelmagalhaes1@live.com>
2023-11-29 08:08:57 -0300

    feat: create logic to filter search strings

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,30 @@
+import { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SearchBarContainer } from './Styles';
+import ProductContext from '../../context/ProductContext';
 
 function SearchBar() {
+  const [search, setSearch] = useState('');
+  const { filterByString } = useContext(ProductContext);
+
+  const handleSearch = () => filterByString(search);
+
+  const handleEnter = (e: any) => {
+    if (e.key === 'Enter') handleSearch();
+  };
+
   return (
     <SearchBarContainer>
       <input
         type="text"
         placeholder="Buscar..."
+        value={ search }
+        onChange={ (e) => setSearch(e.target.value) }
+        onKeyDown={ handleEnter }
       />
       <FontAwesomeIcon
+        onClick={ handleSearch }
         icon={ faSearch }
       />
     </SearchBarContainer>
