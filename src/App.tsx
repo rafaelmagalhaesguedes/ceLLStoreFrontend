162f294edf7d1f30032bb059c489aa9120512685
Rafael <rafaelmagalhaes1@live.com>
2023-11-30 13:02:52 -0300

    feat: create route product-details/:id

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={ <Layout /> }>
         <Route index element={ <Home /> } />
         <Route path="/shopping-cart" element={ <ShoppingCart /> } />
+        <Route path="/product-details/:id" element={ <ProductDetails /> } />
       </Route>
     </Routes>
   );
