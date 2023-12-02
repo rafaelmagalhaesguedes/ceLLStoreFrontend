f25f21ece1c8748e7d96771d794dd5aec4dbd49e
Rafael <rafaelmagalhaes1@live.com>
2023-12-02 17:22:58 -0300

    fix: rename Navbar to Nav

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
-import NavBar from '../../components/NavBar/NavBar';
+import Nav from '../../components/Nav/Nav';
 import Product from '../../components/Product/Product';
 import { Container, Main } from './Styles';
 
 function Home() {
   return (
     <Container>
-      <NavBar />
+      <Nav />
       <Main>
         <Product />
       </Main>
