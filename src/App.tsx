405bbeb274a595c9255b009349c89c80e29d4535
Rafael <rafaelmagalhaes1@live.com>
2023-12-02 00:33:41 -0300

    feat: add route Login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
+import Login from './pages/Login/Login';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Route index element={ <Home /> } />
         <Route path="/shopping-cart" element={ <ShoppingCart /> } />
         <Route path="/product-details/:id" element={ <ProductDetails /> } />
+        <Route path="/login" element={ <Login /> } />
       </Route>
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
