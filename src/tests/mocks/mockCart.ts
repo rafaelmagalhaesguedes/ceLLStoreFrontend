df758963ce0608e02321742ead8f72d2facd6b58
Rafael <rafaelmagalhaes1@live.com>
2023-12-03 09:08:36 -0300

    feat: create mockCart.ts

---
+++ src/tests/mocks/mockCart.ts
@@ -0,0 +1,31 @@
+//
+export const mockCart = [
+  {
+    id: '1',
+    title: 'Product 1',
+    price: 10,
+    thumbnail: 'https://example.com/product1.jpg',
+    condition: 'new',
+    available_quantity: 100,
+    quantity: 1,
+    attributes: [],
+    sold_quantity: 50,
+    shipping: {
+      free_shipping: true,
+    },
+  },
+  {
+    id: '2',
+    title: 'Product 2',
+    price: 11,
+    thumbnail: 'https://example.com/product2.jpg',
+    condition: 'new',
+    available_quantity: 1001,
+    quantity: 1,
+    attributes: [],
+    sold_quantity: 50,
+    shipping: {
+      free_shipping: true,
+    },
+  },
+];
