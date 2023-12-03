ebbba63bd389f305e9ef8bb88c1d49deddb7f9e0
Rafael <rafaelmagalhaes1@live.com>
2023-12-03 11:23:42 -0300

    refact: Header test

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -6,33 +6,31 @@ import ProductContext from '../context/ProductContext';
 import { mockCart } from './mocks/mockCart';
 
 describe('Testing the Header.tsx file', () => {
-  it('should render the logo', () => {
-    renderWithRouter(<Header />);
-    const logo = screen.getByRole('link', { name: /cell store/i });
-    expect(logo).toBeInTheDocument();
-  });
-
-  it('should render the search bar', () => {
-    renderWithRouter(<Header />);
-    const search = screen.getByPlaceholderText(/buscar.../i);
-    expect(search).toBeInTheDocument();
-  });
-
-  it('should render the cart icon', () => {
-    renderWithRouter(<Header />);
-    const cart = screen.getByTestId('icon-cart');
-    expect(cart).toBeInTheDocument();
-  });
-
-  it('should render correct cart count', () => {
+  beforeEach(() => {
     mockContext.cart = mockCart as never[];
-
     renderWithRouter(
       <ProductContext.Provider value={ mockContext }>
         <Header />
       </ProductContext.Provider>,
     );
+  });
 
+  it('should render the logo', () => {
+    const logo = screen.getByRole('link', { name: /cell store/i });
+    expect(logo).toBeInTheDocument();
+  });
+
+  it('should render the search bar', () => {
+    const search = screen.getByPlaceholderText(/buscar.../i);
+    expect(search).toBeInTheDocument();
+  });
+
+  it('should render the cart icon', () => {
+    const cart = screen.getByTestId('icon-cart');
+    expect(cart).toBeInTheDocument();
+  });
+
+  it('should render correct cart count', () => {
     const cartCount = screen.getByTestId('cart-count');
     expect(cartCount.textContent).toBe('2');
   });
